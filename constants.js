9afc2260d76d3a678cd02eef6ebcfb96f2f3ce98
Daniel Johnson <69827271+danielrjc@users.noreply.github.com>
2022-08-19 15:57:28 -0700

    Delete constants.js

--- constants.js
+++
@@ -1,8 +0,0 @@
-width = 10
-height = 5
-x_velocity = 1
-y_velocity = 0
-x=Math.floor(Math.random() * (canvas.width /2))
-y= Math.floor(Math.random() * (canvas.height/2))
-x_food=Math.floor(Math.random() * (canvas.width /2))
-y_food= Math.floor(Math.random() * (canvas.height/2))
